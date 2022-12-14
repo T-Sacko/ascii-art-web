f82eca088591d4a9409ec43a381c4338513c82fe
titi <sackotiti@gmail.com>
2022-12-14 15:44:02 +0000

    asciiweb

--- asciiart/main.go
+++ asciiart/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
+	"Gos"
 	"fmt"
 	"html/template"
 	"net/http"
-"Gos"
-	
 )
 
 var tmpl *template.Template
@@ -34,14 +33,27 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 func Ascii(w http.ResponseWriter, r *http.Request) {
 	// http.NotFound(w, r)
 	if r.URL.Path != "/ascii" {
+
 		w.WriteHeader(http.StatusNotFound)
 		tmpl, _ = template.ParseFiles("404.html")
-	} else {
-		tmpl, _ = template.ParseFiles("index.html")
 	}
 	fmt.Println(r.URL.Path)
 	text := r.FormValue("message")
 	font := r.FormValue("fonts")
 	data := Gos.Asciii(font, text)
+	if data == "no" {
+		w.WriteHeader(http.StatusBadRequest)
+		tmpl, _ = template.ParseFiles("400.html")
+	} else if data == "nop" {
+		w.WriteHeader(http.StatusInternalServerError)
+		tmpl, _ = template.ParseFiles("500.html")
+	} else {
+		tmpl, _ = template.ParseFiles("index.html")
+	}
+
+	// if data == "nop" {
+	// 	w.WriteHeader(http.StatusInternalServerError)
+	// 	tmpl, _ = template.ParseFiles("500.html")
+	// }
 	tmpl.Execute(w, data)
 }
